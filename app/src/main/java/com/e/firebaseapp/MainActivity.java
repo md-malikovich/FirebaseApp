ee8ac33752424d0e61079689130d545466ba5639
Malikovich <md.malikovich@gmail.com>
2019-10-22 23:08:56 +0600

    +1

--- app/src/main/java/com/e/firebaseapp/MainActivity.java
+++ app/src/main/java/com/e/firebaseapp/MainActivity.java
@@ -3,7 +3,10 @@ package com.e.firebaseapp;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
@@ -14,10 +17,12 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.internal.Constants;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -65,11 +70,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //TODO #1
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.menu, menu);
-//        return true;
-//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_signOut) {
+            Intent intent = new Intent(this, PhoneActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     private void getUserInfo() {
         FirebaseFirestore.getInstance().collection("users")
